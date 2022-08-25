827075ee32b98c2ad2ba03beca724cd8874b57c0
Anahid <anahidhemmat2130@gmail.com>
2022-08-25 16:45:16 +0400

    App Component  Header Added

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,10 +1,14 @@
+import AppHeader from "./components/AppHeader";
 import PageTitle from "./components/PageTitle";
-// import style from "./styles/modules/app.module.scss";
+import styles from "./styles/modules/app.module.scss";
 
 function App() {
   return (
     <div className="container">
       <PageTitle>TODO LIST</PageTitle>
+      <div className={styles.app__wrapper}>
+        <AppHeader />
+      </div>
     </div>
   );
 }
